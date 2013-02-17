d9116a061cb679561a6deacec075841bf31316dd
mezbaur <mezbaur@yahoo.co.uk>
2013-02-17 08:28:06 -0800

    Create ssh_exec.py
    1. import paramiko module
    2. define three variables
    3. create SSHClient object
    4. load host keys, it comes from known_hosts file
    5. connect to the SSH server

---
+++ ssh_exec.py
@@ -0,0 +1,22 @@
+## SSH
+# Connecting to an SSH server and remotely executing a command
+
+#!/usr/bin/env python
+
+import paramiko
+
+hostname = '192.168.1.15'
+port = 22
+username = 'mezbaur'
+password = ''
+
+if __name__ == '__main__':
+  paramiko.util.log_to_file('paramiko.log')
+  s = paramiko.SSHClient()
+  s.load_system_hostkeys()
+  s.connect(hostname,port,username,password)
+  stdin, stdout, stderrr = s.exec_command('ifconfig')
+  print stdout.read()
+  s.close()
+
+
